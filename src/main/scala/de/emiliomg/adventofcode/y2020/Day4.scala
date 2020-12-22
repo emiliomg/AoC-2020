b542bb66ca10f6946e1ef17040dc5096b7d071eb
Emilio Markgraf <emilio.markgraf@fashion-digital.de>
2020-12-22 15:17:36 +0100

    Day4 Star2

--- src/main/scala/de/emiliomg/adventofcode/y2020/Day4.scala
+++ src/main/scala/de/emiliomg/adventofcode/y2020/Day4.scala
@@ -5,10 +5,80 @@ import NoWhitespace._
 
 object Day4 {
   def star1(data: String): Int = {
+    def validation(content: Seq[PassportContent]): Seq[Boolean] = {
+      val mandatoryFields = Map(
+        BirthYear      -> false,
+        IssueYear      -> false,
+        ExpirationYear -> false,
+        Height         -> false,
+        HairColor      -> false,
+        EyeColor       -> false,
+        PassportId     -> false
+      )
+
+      val checked = content.foldLeft(mandatoryFields) { (check, c) =>
+        c match {
+          case BirthYear(_)      => check.updated(BirthYear, true)
+          case IssueYear(_)      => check.updated(IssueYear, true)
+          case ExpirationYear(_) => check.updated(ExpirationYear, true)
+          case Height(_, _)      => check.updated(Height, true)
+          case HairColor(_)      => check.updated(HairColor, true)
+          case EyeColor(_)       => check.updated(EyeColor, true)
+          case PassportId(_)     => check.updated(PassportId, true)
+          case _                 => check
+        }
+      }
+
+      checked.values.toSeq
+    }
+
+    runData(data, validation)
+  }
+
+  def star2(data: String): Int = {
+    def validation(content: Seq[PassportContent]): Seq[Boolean] = {
+      val mandatoryFields = Map(
+        BirthYear      -> false,
+        IssueYear      -> false,
+        ExpirationYear -> false,
+        Height         -> false,
+        HairColor      -> false,
+        EyeColor       -> false,
+        PassportId     -> false
+      )
+
+      val checked = content.foldLeft(mandatoryFields) { (check, c) =>
+        c match {
+          case BirthYear(y) if y.length == 4 && 1920.to(2002).contains(y.toInt) =>
+            check.updated(BirthYear, true)
+          case IssueYear(y) if y.length == 4 && 2010.to(2020).contains(y.toInt) =>
+            check.updated(IssueYear, true)
+          case ExpirationYear(y) if y.length == 4 && 2020.to(2030).contains(y.toInt) =>
+            check.updated(ExpirationYear, true)
+          case Height(len, unit) if unit == "cm" && 150.to(193).contains(len.toInt) =>
+            check.updated(Height, true)
+          case Height(len, unit) if unit == "in" && 59.to(76).contains(len.toInt) =>
+            check.updated(Height, true)
+          case HairColor(color) if color.charAt(0) == '#' && color.length == 7 =>
+            check.updated(HairColor, true)
+          case EyeColor(color) if List("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(color) =>
+            check.updated(EyeColor, true)
+          case PassportId(pid) if pid.length == 9 =>
+            check.updated(PassportId, true)
+          case _ => check
+        }
+      }
+
+      checked.values.toSeq
+    }
+
+    runData(data, validation)
+  }
+
+  private def runData(data: String, validation: Seq[PassportContent] => Seq[Boolean]) = {
     parse(data, parseBatch(_)) match {
       case Parsed.Success(result, _) =>
-//        println(result)
-        result.map(_.isValid).count(identity)
+        result.map(_.isValid(validation)).count(identity)
       case Parsed.Failure(_, _, extra) =>
         throw new Exception(extra.trace().longAggregateMsg)
     }
@@ -33,57 +103,32 @@ object Day4 {
   def passportContent[_: P]: P[PassportContent] =
     expirationYear | birthYear | issueYear | height | hairColor | eyeColor | passportId | countryId
 
-  def intNum[_: P]: P[Int]   = CharIn("0-9").rep(1).!.map(Integer.parseInt)
-  def color[_: P]: P[String] = ("#".? ~ CharIn("0-9a-fA-F").rep(min = 6, max = 6) | CharIn("a-zA-Z").rep(1)).!
+  def intNum[_: P]: P[String] = CharIn("0-9").rep(1).!
+  def color[_: P]: P[String] =
+    ("#".?.! ~ CharIn("0-9a-fA-F").rep(min = 6, max = 6) | CharIn("a-zA-Z").rep(1)).!
 
   def birthYear[_: P]: P[BirthYear]           = P("byr:" ~ intNum.map(BirthYear))
   def issueYear[_: P]: P[IssueYear]           = P("iyr:" ~ intNum.map(IssueYear))
   def expirationYear[_: P]: P[ExpirationYear] = P("eyr:" ~ intNum.map(ExpirationYear))
-  def height[_: P]: P[Height]                 = P("hgt:" ~ intNum.map(Height) ~ CharIn("a-zA-Z").rep.?)
-  def hairColor[_: P]: P[HairColor]           = P("hcl:" ~ color.!.map(HairColor))
+  def height[_: P]: P[Height]                 = P("hgt:" ~ (intNum ~ CharIn("a-zA-Z").rep.?.!).map(x => Height(x._1, x._2)))
+  def hairColor[_: P]: P[HairColor]           = P("hcl:" ~ color.map(HairColor))
   def eyeColor[_: P]: P[EyeColor]             = P("ecl:" ~ color.!.map(EyeColor))
   def passportId[_: P]: P[PassportId]         = P("pid:" ~ CharIn("0-9a-zA-Z#").rep(1).!.map(PassportId))
   def countryId[_: P]: P[CountryId]           = P("cid:" ~ CharIn("0-9").rep(1).!.map(CountryId))
 
-  // todo How to do this with types?
   case class Passport(content: Seq[PassportContent]) {
-    def isValid: Boolean = {
-      val mandatoryFields = Map(
-        BirthYear      -> false,
-        IssueYear      -> false,
-        ExpirationYear -> false,
-        Height         -> false,
-        HairColor      -> false,
-        EyeColor       -> false,
-        PassportId     -> false
-//        CountryId      -> false
-      )
-
-      val checked = content.foldLeft(mandatoryFields) { (check, c) =>
-        c match {
-          case BirthYear(_)      => check.updated(BirthYear, true)
-          case IssueYear(_)      => check.updated(IssueYear, true)
-          case ExpirationYear(_) => check.updated(ExpirationYear, true)
-          case Height(_)         => check.updated(Height, true)
-          case HairColor(_)      => check.updated(HairColor, true)
-          case EyeColor(_)       => check.updated(EyeColor, true)
-          case PassportId(_)     => check.updated(PassportId, true)
-//          case CountryId(_)      => check.updated(CountryId, true) // todo see exception
-          case _ => check
-        }
-      }
-
-      checked.values.forall(_ == true)
+    def isValid(f: Seq[PassportContent] => Seq[Boolean]): Boolean = {
+      f(content).forall(_ == true)
     }
   }
 
   sealed trait PassportContent
-  case class BirthYear(year: Int)      extends PassportContent
-  case class IssueYear(year: Int)      extends PassportContent
-  case class ExpirationYear(year: Int) extends PassportContent
-  case class Height(height: Int)       extends PassportContent // ignoring the unit
-  case class HairColor(rgb: String)    extends PassportContent
-  case class EyeColor(color: String)   extends PassportContent
-  case class PassportId(id: String)    extends PassportContent
-  case class CountryId(id: String)     extends PassportContent
+  case class BirthYear(year: String)              extends PassportContent
+  case class IssueYear(year: String)              extends PassportContent
+  case class ExpirationYear(year: String)         extends PassportContent
+  case class Height(height: String, unit: String) extends PassportContent
+  case class HairColor(color: String)             extends PassportContent
+  case class EyeColor(color: String)              extends PassportContent
+  case class PassportId(id: String)               extends PassportContent
+  case class CountryId(id: String)                extends PassportContent
 }
--- src/test/scala/de/emiliomg/adventofcode/y2020/Day4Test.scala
+++ src/test/scala/de/emiliomg/adventofcode/y2020/Day4Test.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 
 class Day4Test extends AnyFlatSpec with Matchers {
   "Star 1" should "work with test input" in {
-    Day4.star1(getTestInput) shouldEqual 2
+    Day4.star1(getTestInputDay1) shouldEqual 2
   }
 
   it should "work with the puzzle input" in {
@@ -15,18 +15,22 @@ class Day4Test extends AnyFlatSpec with Matchers {
     println(s"Star1 result = $result")
     result shouldEqual 264
   }
-//
-//  "Star 2" should "work with the test input" in {
-//    Day4.star2(getTestInput) shouldEqual ???
-//  }
-//
-//  it should "work with the puzzle input" in {
-//    val result = Day4.star2(getPuzzleInput)
-//    println(s"Star2 result = $result")
-//    result shouldEqual ???
-//  }
 
-  def getTestInput: String =
+  "Star 2" should "work with the valid test input" in {
+    Day4.star2(getTestInputDay2_valid) shouldEqual 4
+  }
+
+  it should "work with the invalid test input" in {
+    Day4.star2(getTestInputDay2_invalid) shouldEqual 0
+  }
+
+  it should "work with the puzzle input" in {
+    val result = Day4.star2(getPuzzleInput)
+    println(s"Star2 result = $result")
+    result shouldEqual 224
+  }
+
+  def getTestInputDay1: String =
     """ecl:gry pid:#d50a43 eyr:2020 hcl:#fffffd
       |byr:1937 iyr:2017 cid:147 hgt:183cm
       |
@@ -40,6 +44,35 @@ class Day4Test extends AnyFlatSpec with Matchers {
       |
       |hcl:#cfa07d eyr:2025 pid:166559648
       |iyr:2011 ecl:brn hgt:59in""".stripMargin
+
+  def getTestInputDay2_invalid: String =
+    """eyr:1972 cid:100
+      |hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+      |
+      |iyr:2019
+      |hcl:#602927 eyr:1967 hgt:170cm
+      |ecl:grn pid:012533040 byr:1946
+      |
+      |hcl:dab227 iyr:2012
+      |ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+      |
+      |hgt:59cm ecl:zzz
+      |eyr:2038 hcl:74454a iyr:2023
+      |pid:3556412378 byr:2007""".stripMargin
+
+  def getTestInputDay2_valid: String =
+    """pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      |hcl:#623a2f
+      |
+      |eyr:2029 ecl:blu cid:129 byr:1989
+      |iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+      |
+      |hcl:#888785
+      |hgt:164cm byr:2001 iyr:2015 cid:88
+      |pid:545766238 ecl:hzl
+      |eyr:2022
+      |
+      |iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719""".stripMargin
 
   def getPuzzleInput: String = Source.fromResource("day4.txt").getLines().mkString("\n")
 }
